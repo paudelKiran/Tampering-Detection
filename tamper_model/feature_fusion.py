a7851140e8b970618a0c3e9878501c9166d7fa3e
CresthaRaman <raman.080bct031@tcioe.edu.np>
2026-03-10 15:51:25 +0545

    updated feature

--- tamper_model/feature_fusion.py
+++ tamper_model/feature_fusion.py
@@ -6,13 +6,13 @@ def upsample(x, scale=2):
 def fuse_noise(Dn1, Dn2, Dn3, Dn4):
 
     x = upsample(Dn4)
-    x = tf.concat([x, Dn3], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dn3])
 
     x = upsample(x)
-    x = tf.concat([x, Dn2], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dn2])
 
     x = upsample(x)
-    x = tf.concat([x, Dn1], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dn1])
 
     return x
 
@@ -20,25 +20,25 @@ def fuse_noise(Dn1, Dn2, Dn3, Dn4):
 def fuse_texture_frequency(Dt1, Dt2, Dt3, Dt4,
                            Df2, Df3, Df4):
 
-    Dtf2 = tf.concat([Dt2, Df2], axis=-1)
-    Dtf3 = tf.concat([Dt3, Df3], axis=-1)
-    Dtf4 = tf.concat([Dt4, Df4], axis=-1)
+    Dtf2 = tf.keras.layers.Concatenate(axis=-1)([Dt2, Df2])
+    Dtf3 = tf.keras.layers.Concatenate(axis=-1)([Dt3, Df3])
+    Dtf4 = tf.keras.layers.Concatenate(axis=-1)([Dt4, Df4])
 
     x = upsample(Dtf4)
-    x = tf.concat([x, Dtf3], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dtf3])
 
     x = upsample(x)
-    x = tf.concat([x, Dtf2], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dtf2])
 
     x = upsample(x)
-    x = tf.concat([x, Dt1], axis=-1)
+    x = tf.keras.layers.Concatenate(axis=-1)([x, Dt1])
 
     return x
 
 
 def final_fusion(Dn, Dtf):
 
-    fused = tf.concat([Dn, Dtf], axis=-1)
+    fused = tf.keras.layers.Concatenate(axis=-1)([Dn, Dtf])
 
     fused = tf.keras.layers.Conv2D(
         256, 3, padding='same', activation='relu')(fused)
